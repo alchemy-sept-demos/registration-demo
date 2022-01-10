006bb735808690077d18bdadaa4f23f58174b3b6
Julie Nisbet <jmnisbet@gmail.com>
2022-01-10 10:10:27 -0800

    starting code

---
+++ src/views/Auth/SignIn.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AuthForm from '../../components/Auth/AuthForm';
+
+export default function SignIn() {
+  return (
+    <div>
+      <AuthForm />
+    </div>
+  );
+}
