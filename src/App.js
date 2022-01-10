1d61158854d2bc6e2c9a221be687e8e98c29124e
Julie Nisbet <jmnisbet@gmail.com>
2022-01-10 10:43:12 -0800

    adding sign in and current user logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 import SignIn from './views/Auth/SignIn';
+import { useState } from 'react';
+import { getUser } from './services/users';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(getUser());
+  console.log('CURRENT USER: ', currentUser);
   return (
     <div className="App">
       <BrowserRouter>
+        <NavLink to="/signin"> Sign In </NavLink>
+        <NavLink to="/signup"> Sign Up </NavLink>
         <Switch>
-          <Route path="/" exact>
+          <Route path="/signin" exact>
             <SignIn />
+          </Route>
+          <Route path="/">
+            {currentUser && <h1>I am Signed In</h1>}
+            {!currentUser && <h1>I am NOT signed In</h1>}
           </Route>
         </Switch>
       </BrowserRouter>
--- src/views/Auth/SignIn.js
+++ src/views/Auth/SignIn.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { signInUser } from '../../services/users';
 import AuthForm from '../../components/Auth/AuthForm';
 
 export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const resp = await signInUser(email, password);
+    console.log(resp);
+  };
   return (
     <div>
-      <AuthForm />
+      <AuthForm
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+        handleSubmit={handleSubmit}
+      />
     </div>
   );
 }
